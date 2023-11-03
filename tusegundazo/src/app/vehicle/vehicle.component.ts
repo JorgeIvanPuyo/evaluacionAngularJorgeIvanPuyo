76180b3d6cabdaa9f8fb112fb1b78bf62f0469d5
JorgeIvanPuyo <ivancho1392@gmail.com>
2023-11-02 20:22:14 -0500

    Add method countVehiclesByMarca and modify html to show the new information

--- tusegundazo/src/app/vehicle/vehicle.component.ts
+++ tusegundazo/src/app/vehicle/vehicle.component.ts
@@ -10,12 +10,33 @@ import { VehicleService } from './vehicle.service';
 export class VehicleComponent implements OnInit {
 
   vehicles: Array<Vehicle> = [];
+  vehicleCountsByMarca: { [key: string]: number } = {};
+  marcaArray: string[] = [];
+
   constructor(private vehicleService: VehicleService) { }
   getVehicles(){
     this.vehicleService.getvehicleList().subscribe(vehicles => {
       this.vehicles = vehicles;
-    })
+      this.countVehiclesByMarca();
+      console.log("conteo de vehiculos por marca");
+      console.log(this.vehicleCountsByMarca);
+    });
   }
+
+  countVehiclesByMarca() {
+
+    this.vehicleCountsByMarca = {};
+
+    this.vehicles.forEach(vehicle => {
+      if (this.vehicleCountsByMarca[vehicle.marca]) {
+        this.vehicleCountsByMarca[vehicle.marca]++;
+      } else {
+        this.vehicleCountsByMarca[vehicle.marca] = 1;
+        this.marcaArray.push(vehicle.marca);
+      }
+    });
+  }
+
   ngOnInit() {
     this.getVehicles();
   }
